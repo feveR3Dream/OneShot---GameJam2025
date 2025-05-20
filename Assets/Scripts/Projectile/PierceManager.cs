579fa6ec7dd9848f6b31f3aeffb1a8414ea78021
xcbin <137880665+xcbin@users.noreply.github.com>
2025-05-20 22:04:03 +0700

    Update PierceManager.cs

--- Assets/Scripts/Projectile/PierceManager.cs
+++ Assets/Scripts/Projectile/PierceManager.cs
@@ -1,7 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PierceManager : MonoBehaviour
 {
+<<<<<<< Updated upstream
 }
+=======
+    public static PierceManager Instance { get; private set; }
+    private int PierceStack;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+    }
+
+    public int GetPierceStack()
+    {
+        return PierceStack;
+    }
+
+    public void SetPierceStack(int newValue)
+    {
+        PierceStack = newValue;
+    }
+}
+>>>>>>> Stashed changes
